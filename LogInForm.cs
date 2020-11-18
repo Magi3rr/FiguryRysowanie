c85597b13d7cd4d375854684cea2b510d95758a9
Magi3rr <74048229+Magi3rr@users.noreply.github.com>
2020-11-18 11:20:44 +0100

    Zarys aplikacji
    Dodałem formularz logowania i formularz główny wraz z grafikami

---
+++ LogInForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HR
+{
+    public partial class LogInForm : Form
+    {
+        public LogInForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCloseLog_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnLogIn_Click(object sender, EventArgs e)
+        {
+            using (MainForm mf = new MainForm())
+            {
+                mf.ShowDialog();
+            }
+        }
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HR
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void menuClick (Button btn)
+        {
+            slidePanel.Top = btn.Top;
+            slidePanel.Height = btn.Height;
+
+        }
+
+        private void btnWorkers_Click(object sender, EventArgs e)
+        {
+            menuClick(btnWorkers);
+        }
+
+        private void btnContracts_Click(object sender, EventArgs e)
+        {
+            menuClick(btnContracts);
+        }
+
+        private void btnSalary_Click(object sender, EventArgs e)
+        {
+            menuClick(btnSalary);
+        }
+
+        private void btnPosition_Click(object sender, EventArgs e)
+        {
+            menuClick(btnPosition);
+        }
+    }
+}
